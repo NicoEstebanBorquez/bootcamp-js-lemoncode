8dab081fd1536f935ebc30e3e5e3635a6cbd2353
NicoEsteban <nicolasedgardo.esteban@vodafone.com>
2024-08-10 08:29:30 +0200

    Comprobar numero

--- javascript-sandbox/src/main.js
+++ javascript-sandbox/src/main.js
@@ -1,2 +1,14 @@
-console.log("Hello from main");
+const generarNumeroAleatorio = () => Math.floor(Math.random() * 101);
+const numeroParaAcertar = generarNumeroAleatorio();
+const inputUsuario = document.getElementById("numero");
 
+const comprobarNumero = () => {
+  if (parseInt(inputUsuario.value) == numeroParaAcertar) {
+    console.log("Has acertado");
+  } else {
+    console.log("No has acertado");
+  }
+};
+
+const botonComprobar = document.getElementById("comprobar");
+botonComprobar.addEventListener("click", comprobarNumero);
